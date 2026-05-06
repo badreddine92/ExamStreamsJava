ac676b6ac091f70f7e230bd4380c2fbea8c42924
Badreddine GUENNOUN <b.guennoun@myskolae.fr>
2026-05-06 15:07:48 +0200

    Partie 1

--- src/exo/Partie1.java
+++ src/exo/Partie1.java
@@ -2,22 +2,38 @@ package exo;
 
 import models.Trip;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Partie1 {
 
+    // Lambdas déclarées en variables
+    Predicate<Trip> isLongTrip = trip -> trip.distanceKm() > 10;
+    Predicate<Trip> isExpensiveTrip = trip -> trip.price() > 20;
+    Predicate<Trip> isBadTrip = trip -> trip.rating() < 3;
+    Predicate<Trip> isRecentTrip = trip -> {
+        LocalDate today = LocalDate.now();
+        LocalDate yesterday = today.minusDays(1);
+        LocalDate tripDate = trip.startTime().toLocalDate();
+        return tripDate.equals(today) || tripDate.equals(yesterday);
+    };
+
     public List<Trip> longAndExpensiveTrips(List<Trip> trips) {
-        // distance > 10km et prix > 20€
-        return List.of();
+        return trips.stream()
+                .filter(isLongTrip.and(isExpensiveTrip))
+                .toList();
     }
 
     public List<Trip> badTrips(List<Trip> trips) {
-        // rating < 3
-        return List.of();
+        return trips.stream()
+                .filter(isBadTrip)
+                .toList();
     }
 
     public List<Trip> recentTrips(List<Trip> trips) {
-        // aujourd’hui ou hier
-        return List.of();
+        return trips.stream()
+                .filter(isRecentTrip)
+                .toList();
     }
 }
